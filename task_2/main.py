6d1dc60245e5e4e7b4c78232016ffeb8af6adbbe
yana <yana.rafikova2405@mail.ru>
2024-02-15 09:34:11 +0300

    Add task 2

---
+++ task_2/main.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup
+import pymorphy2
+import re
+import os
+from nltk.corpus import stopwords
+import nltk
+
+# Загрузка списка стоп-слов для русского языка
+nltk.download('stopwords')
+russian_stopwords = stopwords.words('russian')
+
+# Инициализация анализатора
+morph = pymorphy2.MorphAnalyzer()
+
+
+# Функция для очистки и лемматизации текста
+def process_text(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        soup = BeautifulSoup(file.read(), 'html.parser')
+        text = soup.get_text()
+    # Токенизация текста
+    words = re.findall(r'\w+', text)
+    # Очистка списка слов от стоп-слов и неалфавитных символов
+    cleaned_words = [word for word in words if word.isalpha() and word not in russian_stopwords]
+    lemmas = {}  # Словарь для хранения лемм и соответствующих им слов
+    for word in cleaned_words:
+        lemma = morph.parse(word)[0].normal_form  # Лемматизация слова
+        if lemma not in lemmas:
+            lemmas[lemma] = set()
+        lemmas[lemma].add(word)
+    return lemmas
+
+
+directory_path = '../task_1/pages/'
+output_tokens_path = 'tokens.txt'
+output_lemmas_path = 'lemmas.txt'
+
+# Словарь для сбора всех лемм из всех файлов
+all_lemmas = {}
+
+for i in range(150):  # Обработка каждого файла в директории
+    file_path = os.path.join(directory_path, f'page_{i}.txt')
+    lemmas = process_text(file_path)
+    # Обновление глобального словаря лемм
+    for lemma, tokens in lemmas.items():
+        if lemma not in all_lemmas:
+            all_lemmas[lemma] = set()
+        all_lemmas[lemma].update(tokens)
+
+# Запись уникальных токенов
+with open(output_tokens_path, 'w', encoding='utf-8') as tokens_file:
+    unique_tokens = set()
+    for tokens in all_lemmas.values():
+        unique_tokens.update(tokens)  # Сбор уникальных токенов
+    for token in sorted(unique_tokens):
+        tokens_file.write(token + '\n')
+
+# Запись лемм и соответствующих токенов
+with open(output_lemmas_path, 'w', encoding='utf-8') as lemmas_file:
+    for lemma, tokens in sorted(all_lemmas.items()):
+        lemmas_file.write(f"{lemma} {' '.join(sorted(tokens))}\n")
